04984d42330aebd44ebe95ded29d806b19b0a587
nestor <nestor2502@ciencias.unam.mx>
2019-04-15 22:26:16 -0500

    se agrega elimina correcto falta rebalancear

--- Pruebas/src/ArbolRojinegro.java
+++ Pruebas/src/ArbolRojinegro.java
@@ -86,10 +86,115 @@ public class ArbolRojinegro <T extends Comparable<T>>{
         
          }
          //VerticeRojinegro nes = (VerticeRojinegro )buscaVertice(elemento);
-      rebalanceo(nuevo);
+      rebalanceo1(nuevo);
       elementos++;
 	
     }
+
+     public boolean elimina(T j) {
+	//Revisa que de verdad exista el nodo
+	if(busca(j) == false)
+		return false;
+	VerticeRojinegro aux = buscaVertice(j);
+	if(raiz == null) {
+		return false;
+	}
+//------------------------------------------------------------------------------
+	//No tiene hijos
+	if(aux.izquierdo == null && aux.derecho == null) {
+		if(aux == raiz) {
+			raiz = null;
+			elementos--;
+			return true;
+		}
+		
+		if(aux.padre.izquierdo == aux)
+			aux.padre.izquierdo = null;
+		else
+			aux.padre.derecho = null;
+		return true;
+	
+	}
+//-------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
+//Tiene un nodo hijo
+	//Caso izquierdo
+	if(aux.derecho == null && aux.izquierdo != null) {
+		if(aux == raiz) { //Caso especial
+			raiz = raiz.izquierdo;
+			elementos--;
+			return true;
+		}
+		
+		if(aux.padre.derecho == aux) {
+		aux.izquierdo.padre =aux.padre;
+		aux.padre.derecho = aux.izquierdo;
+		}else {
+		aux.izquierdo.padre = aux.padre;
+		aux.padre.izquierdo = aux.izquierdo;
+		
+		}
+//		aux.elemento = aux.izquierdo.elemento;
+//		aux.izquierdo.padre = aux.padre;
+//		aux.izquierdo = null;
+		elementos--;
+		return true;
+
+	
+
+	}
+	//Caso derecho
+//--------------------------------------------------------------------	
+	if(aux.izquierdo == null && aux.derecho != null) {
+		if(aux == raiz) {	//Caso especial
+			raiz = raiz.derecho;
+			elementos--;
+			return true;
+		}
+		if(aux.padre.derecho == aux) {
+		aux.derecho.padre =aux.padre;
+		aux.padre.derecho = aux.derecho;
+		}else {
+		aux.derecho.padre = aux.padre;
+		aux.padre.izquierdo = aux.derecho;
+		
+		}
+//		aux.elemento = aux.derecho.elemento;
+//		aux.derecho.padre = aux.padre;
+//		aux.derecho = null;
+		elementos--;
+		return true;
+
+	}
+//--------------------------------------------------------------------
+	//Caso tiene 2 hijos
+	if(aux.izquierdo != null && aux.derecho != null) {
+		VerticeRojinegro predecesor = predecesor(aux.elemento);	
+//Caso raiz(especial)		
+		if(aux == raiz) { //Se borra la raiz
+			if(predecesor == raiz.izquierdo){ //Caso donde el predecesor esta a la ziquierda de la raiz
+				raiz.izquierdo = null;
+				raiz.elemento = predecesor.elemento;
+			}else {//Caso donde el predecesor no esta a la izquierda de la raiz, 1)donde el predecesor no tiene hijo izquierdo, 2)predecesor tiene hijo izquierdo
+				raiz.elemento = predecesor.elemento;
+				if(predecesor.izquierdo != null){//Tiene hijo izquierdo
+				predecesor.izquierdo.padre = predecesor.padre;
+				predecesor.padre.derecho = predecesor.izquierdo;
+			}else//No tiene hijo izquierdo
+				predecesor.padre.derecho = null;
+			}
+		}else{//Caso General
+			//Hy dos casos, el predecesor tiene hijo izquierdo null o no
+			T temporal = predecesor.elemento;
+			elimina(predecesor.elemento);
+			aux.elemento = temporal;
+		}		
+		elementos--;
+		return true;
+	}
+//Fin del programa	
+	return false;
+     }
 
      public void conectaIzquierda(VerticeRojinegro padre, VerticeRojinegro hijo){ 
               
@@ -243,7 +348,7 @@ public class ArbolRojinegro <T extends Comparable<T>>{
     }
 
 
-    public void rebalanceo(VerticeRojinegro v){
+    public void rebalanceo1(VerticeRojinegro v){
 
          //if(v instanceof ArbolRojinegro.VerticeRojinegro){
 
@@ -294,7 +399,7 @@ public class ArbolRojinegro <T extends Comparable<T>>{
                  t.color = Color.NEGRO;
                  a.color = Color.ROJO;
 
-                 rebalanceo(a);
+                 rebalanceo1(a);
                  return;
 
              }}
@@ -476,6 +581,73 @@ public class ArbolRojinegro <T extends Comparable<T>>{
     }
     }
 
+    public VerticeRojinegro buscaVertice(T x){
+	     VerticeRojinegro otroNodo = raiz; 	
+	
+	     while(otroNodo != null){
+		     if(otroNodo.elemento == x) {
+			     return otroNodo;
+		     }
+		     else if(x.compareTo(otroNodo.elemento) <= 0) {
+			     otroNodo = otroNodo.izquierdo;
+		     }
+		     else {
+			     otroNodo = otroNodo.derecho;
+		     }
+	     }
+	
+	return null;
+
+}
+     
+      public VerticeRojinegro predecesor(T y) {
+	      VerticeRojinegro encontrado = buscaVertice(y);
+	     //arbol vacio
+	     if(encontrado == null)
+		     return null;
+	     //revisamos que su izquierda no este vacia y buscamos el maximo en ese subarbol
+	     if(encontrado.izquierdo != null) {
+	         return maximo(encontrado.izquierdo.elemento);
+	     }
+	     else {
+		 //Si no tiene arbol izquierdo, entonces subimos y buscamos a alguien que si lo tenga
+	     VerticeRojinegro aux = encontrado.padre;
+	     while(aux != null && encontrado == aux.izquierdo) {
+		     encontrado = aux;
+		     aux = aux.padre;
+	     }
+	
+	     if(aux == null)
+		     return null;
+	     else 
+		     return aux;
+	     }
+	
+         }
+     
+     public VerticeRojinegro maximo(T z) {
+
+	     VerticeRojinegro encontrado = buscaVertice(z);
+	     while(encontrado.derecho != null) {
+		     encontrado = encontrado.derecho;
+	     }
+	     
+	     return encontrado;
+     }
+
+     public boolean busca(T r) {
+	VerticeRojinegro otroNodo = raiz; //creamos un nodo que va a la raiz
+	while(otroNodo != null) { // mientras no este vacio, seguimos, si no, regresamos vacio
+		if(otroNodo.elemento == r) {//revisamos en el nodo actual
+			return true;
+		}else if( r.compareTo(otroNodo.elemento) <= 0) {//revisamos en su nodo izquierdo
+			otroNodo = otroNodo.izquierdo;
+		}else {
+			otroNodo = otroNodo.derecho;//revisamos en su nodo derecho
+		}
+	}
+	return false;
+}
 
 
 
--- Pruebas/src/Test.java
+++ Pruebas/src/Test.java
@@ -6,18 +6,27 @@ public static void main(String [] args){
      
       ArbolRojinegro <Integer> ryb = new ArbolRojinegro <Integer>();
         ryb.agrega(50);
-	    ryb.agrega(20);
+        ryb.agrega(100);
+        ryb.agrega(75);
+	    //ryb.agrega(20);
 	    //System.out.println(ryb.getColor(raiz2()));
 		//ryb.agrega(60);
-		ryb.agrega(45);
-		ryb.agrega(35);
-		ryb.agrega(25);
+		//ryb.agrega(45);
+		//ryb.agrega(35);
+		//ryb.agrega(25);
 		//ryb.agrega(48);
 		//ryb.agrega(24);
-        //ryb.bfs( i -> System.out.println(i));
+		ryb.bfs( i -> System.out.println(i));
+	
 		//ryb.dfs(1, i -> System.out.println(i));
-		ryb.dfs(2, i -> System.out.println(i));
+		//ryb.dfs(2, i -> System.out.println(i));
 		//ryb.dfs(3, i -> System.out.println(i));
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("se eliminó 75? "+ryb.elimina(75));
+        //ryb.bfs( i -> System.out.println(i));
+        ryb.dfs(1, i -> System.out.println(i));
 	
 
 
