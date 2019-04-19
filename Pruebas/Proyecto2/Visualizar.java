2f6d913ec511313e0e15b19b2c8f6db3f328d5c6
nestor <nestor2502@ciencias.unam.mx>
2019-04-18 22:48:06 -0500

    interfaz casi terminada

--- Pruebas/Proyecto2/Visualizar.java
+++ Pruebas/Proyecto2/Visualizar.java
@@ -8,25 +8,27 @@ public class Visualizar implements ActionListener{
 
      public JFrame ventana1;
      public JFrame ventana2;
+     public JFrame ventana3;
      public JTextField frase, ruta;
-     public JLabel ingresados;
+     public JLabel etiqueta, etiqueta1, etiqueta2, etiqueta3, etiqueta4;
      public JButton ingresar;
-     public JButton anadir, anadir2, regresa1;
+     public JButton anadir, anadir2, regresa1, regresa2;
      public JButton mostrar;
      public JButton limpiar;
+     public String fraseIng="";
+
 
 
      public Visualizar(){
 
      	iniciaComponentes1();
      	iniciaComponentes2();
+     	iniciaComponentes3();
 
      }
 
      public void iniciaComponentes1(){
      	 ventana1 = new JFrame();
-
-
      	 //ventana1.setLayout(null);
 	 	 Toolkit mipantalla = Toolkit.getDefaultToolkit();
 	 	 Dimension tamanoPantalla = mipantalla.getScreenSize();
@@ -42,7 +44,7 @@ public class Visualizar implements ActionListener{
 
 	 	 //frase = new JTextField("                                Ingresa una frase");
 	 	 frase = new JTextField();
-	 	 TextPrompt placeholder = new TextPrompt("Ingresa una frase", frase);
+	 	 TextPrompt placeholder = new TextPrompt("     Ingresa una frase", frase);
 	 	 placeholder.changeAlpha(0.75f);
          placeholder.changeStyle(Font.ITALIC);
       	  //frase.setLocation(10, 50);
@@ -56,13 +58,37 @@ public class Visualizar implements ActionListener{
       	  ventana1.add(ingresar);
 
       	  anadir = new JButton("Añadir documento");
-      	  anadir.setBounds(30, 80, 200, 40);
+      	  anadir.setBounds(30, 100, 200, 40);
+      	  anadir.addActionListener(this);
       	  ventana1.add(anadir);
+
+      	  etiqueta = new JLabel();
+      	  etiqueta.setText("Documentos ingresados");
+          etiqueta.setBounds(30, 180, 400, 40);
+          ventana1.add(etiqueta);
+
+          etiqueta4 = new JLabel();
+          etiqueta4.setText("Documentos ingresados");
+          etiqueta4.setBounds(30, 150, 400, 40);
+          ventana1.add(etiqueta4);
+
+          mostrar = new JButton("Mostrar coincidencias");
+          mostrar.setBounds(30, 320, 200, 40);
+          mostrar.addActionListener(this);
+          ventana1.add(mostrar);
+
+          limpiar = new JButton("Limpiar");
+          limpiar.setBounds(260, 320, 200, 40);
+          limpiar.addActionListener(this);
+          ventana1.add(limpiar);
+
+
+
 
      }
 
      public void iniciaComponentes2(){
-     	 JLabel etiqueta1 = new JLabel();
+     	 etiqueta1 = new JLabel();
          ruta = new JTextField();
          anadir2= new JButton("Añadir");
          regresa1 = new JButton("Regresar");
@@ -74,7 +100,7 @@ public class Visualizar implements ActionListener{
          ventana2.setLayout(null);
          ventana2.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 	 	 ventana2.setTitle("Proyecto 2");
-	 	 ventana2.setVisible(true);
+	 	 ventana2.setVisible(false);
 	 	 ventana2.setResizable(false);
 	 	 etiqueta1.setText("       Ingresa la ruta del archivo:");
 	 	 etiqueta1.setBounds(20, 10, 250, 40);
@@ -85,28 +111,99 @@ public class Visualizar implements ActionListener{
          sombra.changeStyle(Font.ITALIC);
          ventana2.add(ruta);
          anadir2.setBounds(20, 130, 120, 40 );
+         anadir2.addActionListener(this);
          ventana2.add(anadir2);
          regresa1.setBounds(160, 130, 120, 40 );
+         regresa1.addActionListener(this);
          ventana2.add(regresa1);
 
 
      }
+
+     public void iniciaComponentes3(){
+
+     	 String frase3 = fraseIng;
+     	 System.out.println(fraseIng);
+
+     	 ventana3 = new JFrame();
+     	 ventana3.setSize(300, 323);
+     	 ventana3.setLocationRelativeTo(null); // centramos la ventana en la pantalla
+         ventana3.setLayout(null);
+         ventana3.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+	 	 ventana3.setTitle("Proyecto 2");
+	 	 ventana3.setVisible(false);
+	 	 ventana3.setResizable(false);
+
+	 	 etiqueta2 = new JLabel();
+	 	 etiqueta2.setText("Frase ingresada:");
+	 	 etiqueta2.setBounds(20, 20, 200, 40);
+	 	 ventana3.add(etiqueta2);
+
+	 	 etiqueta3 = new JLabel();
+	 	 etiqueta3.setBounds(20, 60, 250, 40);
+	 	 ventana3.add(etiqueta3);
+
+	 	 regresa2 = new JButton("Regresar");
+	 	 regresa2.setBounds(85, 230, 120, 40);
+	 	 regresa2.addActionListener(this);
+	 	 ventana3.add(regresa2);
+
+
+     }
+
      @Override
      public void actionPerformed(ActionEvent e){
 
-     	String frase2 =""; 
+     	 //String frase2 =""; 
 
-     	if(e.getSource()==ingresar){
+     	 if(e.getSource()==ingresar){
 
-     	frase2 = frase.getText();
-     	frase.setEnabled(false);
+     	     //frase2 = frase.getText();
+     	     fraseIng= frase.getText().trim();
+     	     frase.setEnabled(false);
+     	     ingresar.setEnabled(false);
+     	     System.out.println(fraseIng);
 
-     	
+     }
 
-     	System.out.println(frase2);
+      if(e.getSource()==anadir){
 
+         ventana2.setVisible(true);
+     }
+     
+     if(e.getSource()==anadir2){
+     	  String ruta1;
+          ruta1 = ruta.getText();
+     	  ruta.setText("");
+         
 
-     }}
+     }
+
+      if(e.getSource()==regresa1){
+
+      	 ventana2.setVisible(false);
+
+     }
+
+     if(e.getSource()==limpiar){
+
+     	frase.setText("");
+     	frase.setEnabled(true);
+     	ingresar.setEnabled(true);
+     }
+
+     if(e.getSource()==mostrar){
+         
+         etiqueta3.setText("'"+fraseIng+"'");
+     	 ventana3.setVisible(true);
+     }
+
+     if(e.getSource()==regresa2){
+
+     	 ventana3.setVisible(false);
+     }
+
+ }
 
 
 
